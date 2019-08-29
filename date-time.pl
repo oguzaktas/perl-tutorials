c7da9b02b4a92d81c68c2a372a90a03d58ce1148
Oğuz Aktaş <oguzaktass@gmail.com>
2019-08-29 22:27:48 +0300

    Add files via upload

--- date-time.pl
+++ date-time.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 
 # current date and time
-
 @months = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
 @days = qw(Sun Mon Tue Wed Thu Fri Sat Sun);
 ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
@@ -12,11 +11,8 @@ $datestring = gmtime();
 print "GMT date and time $datestring\n\n";
 
 # time(), localtime() and gmtime() functions
-
 $epoc = time();
-
 print "Number of seconds since Jan 1, 1970 - $epoc\n";
-
 $datestring = localtime();
 print "Current date and time $datestring\n";
 $epoc = time();
